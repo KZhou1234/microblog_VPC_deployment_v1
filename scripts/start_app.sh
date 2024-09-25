1229a3cac887ad39bda7b76336b88a386a733414
Ubuntu <ubuntu@ip-10-0-2-98.ec2.internal>
2024-09-25 19:45:25 +0000

    clean the script

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 #set up the server so that has all of the dependencies that the application needs
 sudo apt update && sudo apt install fontconfig openjdk-17-jre software-properties-common && sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt install python3.9 python3.9-venv
-python3.9 -m venv venv
-source venv/bin/activate
 #clone the GH repository
 #git pull https://github.com/kura-labs-org/C5-Deployment-Workload-4.git
 if [ ! -d "microblog_VPC_deployment" ]; then
-	mkdir microblog_VPC_deployment
+	git clone https://github.com/KZhou1234/microblog_VPC_deployment.git
+else
+	git pull https://github.com/KZhou1234/microblog_VPC_deployment.git
 fi
+
 cd microblog_VPC_deployment
-git init
 
-git pull https://github.com/kura-labs-org/C5-Deployment-Workload-4.git
-#git remote add origin https://github.com/KZhou1234/microblog_VPC_deployment.git
-#git branch -M main
-#git push -u origin main
-
+python3.9 -m venv venv
+source venv/bin/activate
+sudo apt update && sudo apt install python-pip
 #install the application dependencies from the requirements.txt file
 pip install -r requirements.txt
 pip install gunicorn pymysql cryptography
 #set ENVIRONMENTAL Variables
-FLASK_APP=microblog.py
+export FLASK_APP=microblog.py
 #flask commands
 flask translate compile
 flask db upgrade
