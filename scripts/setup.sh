d1ee5da0a5dd1563afa5b941f4d7aa00498733cb
Ubuntu <ubuntu@ip-10-0-2-98.ec2.internal>
2024-09-26 06:11:50 +0000

    scripts/

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 #ssh into applicaiton server
-ssh -i app_key.pem ubuntu@10.0.2.98 source start_app.sh
+start_app_url="https://raw.githubusercontent.com/KZhou1234/microblog_VPC_deployment/refs/heads/main/scripts/start_app.sh"
+start_app_path="/home/ubuntu/start_app.sh"
+ssh -i app_key.pem ubuntu@10.0.2.98 "curl -L -o $start_app_path $start_app_url 2>/dev/null && chmod 755 $start_app_path && source $start_app_path"
